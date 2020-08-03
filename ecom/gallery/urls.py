569cc0716419ffecca3251f6c4c47312372b14a0
nimbusskye <alexwang6671@gmail.com>
2020-08-02 23:05:40 -0400

    Client can now create items

--- ecom/gallery/urls.py
+++ ecom/gallery/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
+app_name='gallery'
 urlpatterns=[
     path('', views.index, name='index'),
+    path('upload/', views.upload, name='upload'),
 ]
--- ecom/gallery/views.py
+++ ecom/gallery/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from gallery.models import Item
+from gallery.forms import ItemForm
 
 # Create your views here.
 
 def index(request):
-    item_list=Item.objects.order_by('-name')
+    item_list=Item.objects.order_by('name')
     context={'item_list': item_list}
     return render(request, 'gallery/index.html', context)
-    
+
+def upload(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = Item(name=form.cleaned_data['name'], desc=form.cleaned_data['desc'], cost=form.cleaned_data['price'], pic=request.FILES['pic'])
+            item.save()
+            return HttpResponseRedirect('/gallery/')
+        else:
+            return render(request, 'gallery/errors.html', {'form': form})
+    form = ItemForm()
+    return render(request, 'gallery/upload.html', {'form': form})
