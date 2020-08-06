5f15b6295efbbfbbb91cdd75bf3d0198bdd68367
nimbusskye <alexwang6671@gmail.com>
2020-08-05 21:17:07 -0400

    Added first name, last name, email to registration form

--- ecom/register/views.py
+++ ecom/register/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 # Create your views here.
 
 def register(request):
-    form=UserCreationForm()
+    form=RegisterForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/gallery/')
         else:
-            form=UserCreationForm()
+            form=RegisterForm()
     return render(request, 'register/register.html', {'form': form})
         
