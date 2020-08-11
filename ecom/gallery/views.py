ac351a1505e3a757e0ff07750bad8f2701087008
nimbusskye <alexwang6671@gmail.com>
2020-08-11 16:40:36 -0400

    Cart views code cleanup

--- ecom/gallery/views.py
+++ ecom/gallery/views.py
@@ -26,34 +26,35 @@ def detail (request, id):
     except Item.DoesNotExist:
         item = None
     return render(request, 'gallery/detail.html', {'item': item})
+    
+def cartExists (request):
+    if request.user.is_authenticated and Cart.objects.filter(user=request.user):
+        return True
+    return False
 
 def add_to_cart (request, id):
-    if request.user.is_authenticated:
-        c = Cart.objects.filter(user=request.user)
-        if not c:
-            c = Cart()
-            c.save()
-            request.user.cart.add(c)
-        else:
-            c = c.get()
-        c.addItem(id)
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/gallery/')
+    if not cartExists(request):
+        c = Cart()
+        c.save()
+        request.user.cart.add(c)
+    c = Cart.objects.filter(user=request.user)
+    cart = c.get()
+    cart.addItem(id)
     return HttpResponseRedirect('/gallery/cart/')
     
 def view_cart (request):
-    if request.user.is_authenticated:
+    if cartExists(request):
         c = Cart.objects.filter(user=request.user)
-        if not c:
-            return HttpResponseRedirect('/gallery/')
         cart = c.get()
         items = cart.items.all()
         return render(request, 'gallery/cart.html', {'cart': cart, 'items': items})
-    return HttpResponseRedirect('/gallery/')
+    return render(request, 'gallery/cart.html', {'cart': None, 'items': None})
     
 def clear_cart (request):
-    if request.user.is_authenticated:
+    if cartExists(request):
         c = Cart.objects.filter(user=request.user)
-        if not c:
-            return HttpResponseRedirect('/gallery/')
         cart = c.get()
         cart.clear()
         cart.totalprice=0
@@ -61,19 +62,15 @@ def clear_cart (request):
     return HttpResponseRedirect('/gallery/cart/')
     
 def remove_item (request, id):
-    if request.user.is_authenticated:
+    if cartExists(request):
         c = Cart.objects.filter(user=request.user)
-        if not c:
-            return HttpResponseRedirect('/gallery/')
         cart = c.get()
         cart.removeItem(id)
     return HttpResponseRedirect('/gallery/cart/')
     
 def checkout (request):
-    if request.user.is_authenticated:
+    if cartExists(request):
         c = Cart.objects.filter(user=request.user)
-        if not c:
-            return HttpResponseRedirect('/gallery/')
         cart = c.get()
         cart.checkout()
     return HttpResponseRedirect('/gallery/')
