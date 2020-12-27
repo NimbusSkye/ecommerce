651f711909747c03f9e16ef9a0cd0ade900a50d8
nimbusskye <alexwang6671@gmail.com>
2020-12-26 20:48:17 -0500

    fixed stuff

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1b^kq&-jw4-c^p653k3z@dw@(btv6imsiq=kyfa99xjm7x-8ig'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
